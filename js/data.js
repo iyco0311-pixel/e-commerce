d9c40eba33db307cbfa76a955313080acdcd9b73
John Doe <iyco.0311@gmail.com>
2026-03-09 22:02:55 +0100

    basic data

---
+++ js/data.js
@@ -0,0 +1,10 @@
+export const products = [
+  { id: "p1", name: "Product 1", price: 99 },
+  { id: "p2", name: "Product 2", price: 109 },
+  { id: "p3", name: "Product 3", price: 149 },
+  { id: "p4", name: "Product 4", price: 49 },
+  { id: "p5", name: "Product 5", price: 129 },
+  { id: "p6", name: "Product 6", price: 79 },
+  { id: "p7", name: "Product 7", price: 159 },
+  { id: "p8", name: "Product 8", price: 299 },
+];

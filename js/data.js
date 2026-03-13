1f13532e1169cf35429b4e8f9c8ee0449bbbeae3
John Doe <iyco.0311@gmail.com>
2026-03-13 16:09:06 +0100

    Add some elements

--- js/data.js
+++ js/data.js
@@ -1,10 +1,10 @@
 export const products = [
-  { id: "p1", name: "Product 1", price: 99 },
-  { id: "p2", name: "Product 2", price: 109 },
-  { id: "p3", name: "Product 3", price: 149 },
-  { id: "p4", name: "Product 4", price: 49 },
-  { id: "p5", name: "Product 5", price: 129 },
-  { id: "p6", name: "Product 6", price: 79 },
-  { id: "p7", name: "Product 7", price: 159 },
-  { id: "p8", name: "Product 8", price: 299 },
+  { id: "p1", name: "Apple", price: 99 },
+  { id: "p2", name: "Banana", price: 109 },
+  { id: "p3", name: "Orange", price: 149 },
+  { id: "p4", name: "Grape", price: 49 },
+  { id: "p5", name: "Strawberry", price: 129 },
+  { id: "p6", name: "Blueberry", price: 79 },
+  { id: "p7", name: "Kiwi", price: 159 },
+  { id: "p8", name: "Pineapple", price: 299 },
 ];
